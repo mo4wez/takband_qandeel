e3532923c9259449fe54d4f0a13c4be35a2c3270
moawezz <moawez.de@gmail.com>
2023-10-30 19:54:43 +0330

    Register qandeel models #1

--- qandeel/admin.py
+++ qandeel/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Century, Poet, Book, Poem, Section
+
+@admin.register(Century)
+class CenturyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+
+@admin.register(Poet)
+class PoetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'century', 'created_at']
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'poet', 'created_at']
+
+@admin.register(Poem)
+class PoemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'book', 'poem', 'created_at', 'modified_at']
