2f49e40b455d434016d6d497532ae081f3b898d9
moawezz <moawez.de@gmail.com>
2023-10-31 17:00:50 +0330

    Create Comment Model and fix some indents in models file

--- qandeel/models.py
+++ qandeel/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Century(models.Model):
     name = models.CharField(max_length=255)
@@ -8,7 +8,7 @@ class Century(models.Model):
     class Meta:
         verbose_name_plural = 'Centuries'
 
-    
+
     def __str__(self):
         return self.name
 
@@ -20,7 +20,6 @@ class Poet(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.name
@@ -34,7 +33,6 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.name
 
@@ -45,7 +43,6 @@ class PoeticFormat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.name
 
@@ -55,10 +52,29 @@ class Section(models.Model):
     body = models.TextField()
     book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
     poetic_format = models.OneToOneField(to=PoeticFormat, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    COMMENT_WAITING = 'wa'
+    COMMENT_APPROVED = 'ap'
+    COMMENT_NOT_APPROVED = 'na'
+
+    STATUS_CHOICES = [
+        (COMMENT_WAITING,'Waiting'),
+        (COMMENT_APPROVED,'Approved'),
+        (COMMENT_NOT_APPROVED,'Not Approved'),
+    ]
+
+    section = models.ForeignKey(to=Section, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(to=get_user_model(), related_name='comments', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255, verbose_name='Leave a comment')
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=COMMENT_WAITING)
+    active = models.BooleanField(default=False)
+
