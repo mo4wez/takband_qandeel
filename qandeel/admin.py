8d226b72264c79f8e6ec62291ff26bad457913fd
moawezz <moawez.de@gmail.com>
2023-10-31 17:00:17 +0330

    Register Comment Model

--- qandeel/admin.py
+++ qandeel/admin.py
@@ -1,24 +1,28 @@
 from django.contrib import admin
 
-from .models import Century, Poet, Book, PoeticFormat, Section
+from .models import Century, Poet, Book, PoeticFormat, Section, Comment
+
 
 @admin.register(Century)
 class CenturyAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at']
-    
+    list_display = ['id', 'name', 'created_at',] 
 
 @admin.register(Poet)
 class PoetAdmin(admin.ModelAdmin):
-    list_display = ['name', 'century', 'created_at']
+    list_display = ['id', 'name', 'century', 'created_at',]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['name', 'poet', 'created_at']
+    list_display = ['id', 'name', 'poet', 'created_at',]
 
 @admin.register(PoeticFormat)
 class PoeticFormatAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at']
+    list_display = ['id', 'name', 'created_at',]
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'book', 'poetic_format', 'created_at', 'modified_at']
+    list_display = ['id', 'title', 'book', 'poetic_format', 'created_at', 'modified_at',]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'section', 'status', 'active',]
