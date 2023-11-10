1b175111d8004bb7c71de82baf97315de00ae074
moawezz <moawez.de@gmail.com>
2023-11-10 04:39:16 +0330

    Create PostListView & PostDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+class PostListView(generic.ListView):
+    queryset = Post.objects.filter(active=True, status=Post.POST_PUBLISHED).all()
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = 'title'
+
+
+class PostDetailView(generic.DeleteView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
