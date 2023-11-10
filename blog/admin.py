4ab3a03fd693b27882b0563404d9ef64e90eb249
moawezz <moawez.de@gmail.com>
2023-11-10 04:38:33 +0330

    Register PostModel to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'status', 'active', 'created_at', 'modified_at',]
+    search_fields = ['title',]
+    list_editable = ['status', 'active',]
+    ordering = ['created_at',]
