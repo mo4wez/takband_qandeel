554603551e18afce5233e083bea24fe7ca47703e
moawezz <moawez.de@gmail.com>
2023-11-10 04:40:10 +0330

    add urls to blog

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import PostListView, PostDetailView
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+]
