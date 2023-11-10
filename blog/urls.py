806bc0719a84010b10eed967807d85729f8fb10e
moawezz <moawez.de@gmail.com>
2023-11-10 04:42:44 +0330

    add app_name to blog urls files

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import PostListView, PostDetailView
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
